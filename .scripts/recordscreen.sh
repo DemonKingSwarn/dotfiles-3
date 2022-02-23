7925d54fa37167c3988c493e6603b4e2f9f807bc
DemonKingSwarn <rockingswarn@gmail.com>
2022-02-23 15:44:00 +0530

    few fixes

--- .scripts/recordscreen.sh
+++ .scripts/recordscreen.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 name=$(printf '%s' "$*" | dmenu -i -p "Enter file name:" | tr ' ' '-')
+notify-send "ffmpeg" "Recording started"
 ffmpeg -video_size 1920x1080 -framerate 25 -f x11grab -i :0.0+100,200 "$HOME/vids/recordings/$name"
