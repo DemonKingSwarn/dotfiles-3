abd27e5424a86c9847e7602b2e01f30a1231256b
DemonKingSwarn <rockingswarn@gmail.com>
2022-02-23 16:45:33 +0530

    . e

--- .scripts/recordscreen.sh
+++ .scripts/recordscreen.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-
+path="$HOME/vids/recordings"
 name=$(printf '%s' "$*" | dmenu -i -p "Enter file name:" | tr ' ' '-')
-ffmpeg -f x11grab -r 25 -i :0.0 -f pulse -i alsa_output.pci-0000_00_1b.0.analog-stereo.monitor "$HOME/vids/recordings/$name.mp4"
+ffmpeg -f x11grab -r 25 -i :0.0 -f pulse -i alsa_output.pci-0000_00_1b.0.analog-stereo.monitor "$path/$name.mp4"
+ffmpeg -i "$path/$name.mp4" -f mp4 "$path/$name.mp4" 
 notify-send "ffmpeg" "recording ended"
