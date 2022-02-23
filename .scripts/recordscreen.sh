2f23026e0357ccb8dec63a643c7f6e0d15093c95
DemonKingSwarn <rockingswarn@gmail.com>
2022-02-23 16:33:47 +0530

    added record audio feature

--- .scripts/recordscreen.sh
+++ .scripts/recordscreen.sh
@@ -2,4 +2,4 @@
 
 name=$(printf '%s' "$*" | dmenu -i -p "Enter file name:" | tr ' ' '-')
 notify-send "ffmpeg" "Recording started"
-ffmpeg -f x11grab -r 25 -i :0.0  "$HOME/vids/recordings/$name.mp4"
+ffmpeg -f x11grab -r 25 -i :0.0 -f pulse -i alsa_output.pci-0000_00_1b.0.analog-stereo.monitor "$HOME/vids/recordings/$name.mp4"
