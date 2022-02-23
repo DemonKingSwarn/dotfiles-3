c916e2bd052dad302902dbdb52e1faf4b574c369
DemonKingSwarn <rockingswarn@gmail.com>
2022-02-23 11:54:08 +0530

    changes

--- .scripts/startup.sh
+++ .scripts/startup.sh
@@ -5,7 +5,9 @@ xsetroot -cursor_name left_ptr &
 picom --experimental-backends -f -b &
 sxhkd &
 dunst &
-$HOME/.scripts/set_wallpaper &
+#$HOME/.scripts/set_wallpaper &
+feh --bg-fill $HOME/pix/nordic-wall/ign_mandalorian.jpg &
 $HOME/.config/polybar/launch.sh &
+wal -i $HOME/pix/nordic-wall/ign_mandalorian.jpg &
 flameshot &
 setxkbmap -option ctrl:nocaps
