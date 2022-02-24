7ca89c61926500197255cd167c015652d037aa23
DemonKingSwarn <rockingswarn@gmail.com>
2022-02-24 14:47:57 +0530

    riced the startup script lol

--- .scripts/startup.sh
+++ .scripts/startup.sh
@@ -1,13 +1,27 @@
 #!/usr/bin/env sh
 
-# rice
-xsetroot -cursor_name left_ptr &
-picom --experimental-backends -f -b &
+# This is the startup script.
+
+# cursor settings
+xsetroot -cursor_name left_ptr 
+
+# keyboard settings
+## Turns Caps Lock into Control, useful for Emacs
+setxkbmap -option ctrl:nocaps
 sxhkd &
+
+# system
+flameshot &
+
+# rice
+picom --experimental-backends -f -b &
 dunst &
 #$HOME/.scripts/set_wallpaper &
 feh --bg-fill $HOME/pix/nordic-wall/ign_mandalorian.jpg &
 $HOME/.config/polybar/launch.sh &
 wal -i $HOME/pix/nordic-wall/ign_mandalorian.jpg &
-flameshot &
-setxkbmap -option ctrl:nocaps
+
+while true;
+do
+	sleep 1
+done
