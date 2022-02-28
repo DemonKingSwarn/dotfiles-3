1146308dca98a739b5123c2a672847aa7cea7b93
DemonKingSwarn <rockingswarn@gmail.com>
2022-02-28 19:36:31 +0530

    update ig

--- .scripts/startup.sh
+++ .scripts/startup.sh
@@ -18,7 +18,7 @@ xss-lock -l $HOME/.scripts/lock.sh &
 # rice
 picom --experimental-backends -f -b &
 dunst &
-$HOME/.scripts/setbg/set_wallpaper &
+$HOME/.scripts/setbg/setbg &
 $HOME/.config/polybar/launch.sh &
 
 while true;
