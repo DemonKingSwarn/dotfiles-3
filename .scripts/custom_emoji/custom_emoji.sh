1620c569d9be889a3e511a102a7e03afd0292d7e
DemonKingSwarn <rockingswarn@gmail.com>
2022-03-26 16:16:33 +0530

    autocommit

--- .scripts/custom_emoji/custom_emoji.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR=$HOME/.scripts/custom_emoji/
-
-[ -z "$1" ] && "$SCRIPT_DIR/subscripts/run" || "$SCRIPT_DIR/subscripts/$@"
-
--- .scripts/funStuff/watchLive.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-query=$(cat urls.txt | dmenu -p "Livestream URL: " -i -l 20)
-url=$(youtube-dl -f best --get-url "$query")
-mpv $url
-
-
